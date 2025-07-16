7228a3ad0ffe10ffc22fb549855f2b11ffb056e6
amtulnoor-15 <arsheema619@gmail.com>
2025-07-16 06:26:27 +0000

    new repo-day1

---
+++ Basics-of-JS/use-cases-of-console.js
@@ -0,0 +1,21 @@
+let i = 5+10;
+console.log(i);
+console.log('amtulnoor \n hasanaat');  // \n prints the next term in  next line
+console.log('syed \t effath'); // 
+
+// print your friend name  and her email id , age , pass with data type using multiple line console.log syntax
+
+let name = 'hasanaat';
+let email = 'hasanaat00@gmail.com';
+let age = 24;
+const password = 'gonnamakeit'
+
+console.log(`my name: ${name} 
+    email address: ${email} 
+    age is: ${age} 
+    and password:${password}`);
+
+    console.log(typeof(name));
+    console.log(typeof(email));
+    console.log(typeof(age));
+    console.log(typeof(password));
